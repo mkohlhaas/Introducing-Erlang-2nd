8a863c375ce2b1b3f4f9c3d5e2e81713e41cc1ec
Hans-J. Schmid <knock@myopendoor.de>
2023-02-27 00:40:31 +0100

    reformats source code in Ch. 2.

--- Ch02/ex2-mixing/convert.erl
+++ Ch02/ex2-mixing/convert.erl
@@ -1,6 +1,9 @@
 -module(convert).
--export([mps_to_mph/1, mps_to_kph/1]). 
 
-mps_to_mph(Mps) -> 2.23693629 * Mps.
+-export([mps_to_mph/1, mps_to_kph/1]).
 
-mps_to_kph(Mps) -> 3.6 * Mps.
+mps_to_mph(Mps) ->
+  2.23693629 * Mps.
+
+mps_to_kph(Mps) ->
+  3.6 * Mps.
--- Ch02/ex3-combined/combined.erl
+++ Ch02/ex3-combined/combined.erl
@@ -1,4 +1,7 @@
 -module(combined).
--export([height_to_mph/1]). 
 
-height_to_mph(Meters) -> convert:mps_to_mph(drop:fall_velocity(Meters)).
+-export([height_to_mph/1]).
+
+height_to_mph(Meters) ->
+  convert:mps_to_mph(
+    drop:fall_velocity(Meters)).
